e08414a2cd7ea914ab60e3a35497c6969f4adf5e
Chanathip <gzoneaa@gmail.com>
2023-12-02 00:03:16 +0700

    done project INT305

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import db from './firebase/init.js'
 import UserList from './components/UserList.vue'
 
 const users = ref([])
-
+const posts = ref([])
 async function getUsers(){
   const userRef = collection(db, "users");
     const qry = query(userRef);
@@ -19,8 +19,23 @@ async function getUsers(){
 })
 }
 
+async function getPosts(){
+  const postRef = collection(db, "posts");
+    const qry = query(postRef);
+   
+    const querySnap = await getDocs(qry);
+    querySnap.forEach((doc) => {
+    let data = doc.data();
+    data.id = doc.id;
+    posts.value.push(data);
+})
+}
 onMounted(() => {
     getUsers() 
+    getPosts()
+    console.log(users.value)
+    console.log(posts.value)
+
 })
 </script>
 
--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -4,17 +4,37 @@ import { useRoute } from "vue-router"
 import { collection, query, where, getDocs, onSnapshot } from "firebase/firestore"
 import db from "../firebase/init.js"
 import PostItem from "../components/PostItem.vue"
-
 const user = ref("")
 const posts = ref([])
-const route = useRoute() 
-
-async function getPosts(){
+const route = useRoute()
+const count = ref(0)
+async function getPosts() {
   posts.value = []
   user.value = route.params.user
   const postRef = collection(db, "posts");
-  const qry = query(postRef, where("user","==",user.value));
-  console.log(qry)
+  const qry = query(postRef, where("user", "==", user.value));
+  const unsubscribe = onSnapshot(qry, (querySnap) => {
+    posts.value = []
+    count.value = 0      
+    console.log(querySnap)
+    count.value = querySnap.size
+    querySnap.forEach(async (doc) => {
+       let data = doc.data();
+      data.id = doc.id;
+      const commentRef = collection(db, "posts", doc.id, "comments");
+      const queryComment = await getDocs(query(commentRef));
+      console.log(queryComment)
+      
+      data.comments = []
+      queryComment.forEach((com) => {
+        let comment = com.data();
+        comment.id = com.id;
+        console.log(comment)
+        data.comments.push(comment);
+      })
+      posts.value.push(data);
+    });
+  })
 }
 
 watch( () => route.params.user, getPosts)
@@ -22,12 +42,13 @@ watch( () => route.params.user, getPosts)
 onMounted(() => {
   getPosts() 
   console.log(user.value)
+  console.log(posts.value)
 })
 
 </script>
 
 <template>
-    <h3>Posts : {{user}}</h3>
+  <h3>Posts : {{ user }} </h3>
     <PostItem v-for="post in posts" :post="post" :key="post.id" />
 </template>
 
